304b2965796a6d0feb68be6d3924346311b0dbd1
KKranzz <117629281+KKranzz@users.noreply.github.com>
2023-09-15 20:56:30 +0300

    Delete search-server directory

--- search-server/document.cpp
+++
@@ -1,8 +0,0 @@
-#include "document.h"
-
-
-
-std::ostream& operator<<(std::ostream& out, const Document& doc) {
-    out << "{ document_id = " << doc.id << ", relevance = " << doc.relevance << ", rating = " << doc.rating << " }";
-    return out;
-}

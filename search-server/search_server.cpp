784ecbf28591b937cf21a707a7458b373ea36f07
KKranzz <117629281+KKranzz@users.noreply.github.com>
2023-11-13 20:33:22 +0300

    Update search_server.cpp

--- search-server/search_server.cpp
+++ search-server/search_server.cpp
@@ -93,10 +93,8 @@ vector<string> SearchServer::SplitIntoWordsNoStop(const string& text) const {
     if (ratings.empty()) {
         return 0;
     }
-    int rating_sum = 0;
-    for (const int rating : ratings) {
-        rating_sum += rating;
-    }
+    int rating_sum = accumulate(ratings.begin(), ratings.end(), 0);
+    
     return rating_sum / static_cast<int>(ratings.size());
 }
 
